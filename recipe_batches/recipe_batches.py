c782543b75a125cd8795da8b67f2b8b0ddc655d1
System Administrator <jon.s.solari@gmail.com>
2020-01-22 16:33:28 -0500

    :art: test pass now for recipe
    tests pass now for recipe

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -5,10 +5,13 @@ import math
 def recipe_batches(recipe, ingredients):
     rec = list(recipe.values())
     ing = list(ingredients.values())
+    
+    if list(recipe.keys()) != list(ingredients.keys()):
+        return 0
     res = {rec[i]: ing[i] for i in range(0, len(rec))}
     diff1 = []
     for key, value in res.items():
-        if int(value) // int(key) > 1:
+        if int(value) // int(key) >= 1:
             diff1.append(int(value // int(key)))
     if len(diff1) == len(recipe.keys()):
         return int(min(diff1))
@@ -20,6 +23,6 @@ def recipe_batches(recipe, ingredients):
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 100, 'butter': 50, 'flour': 10 }
+  ingredients = { 'milk': 198, 'butter': 52, 'flour': 10 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
