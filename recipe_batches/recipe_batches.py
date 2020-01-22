641b37d8e8a97a03e7f495011749ecac10d07621
System Administrator <jon.s.solari@gmail.com>
2020-01-22 16:28:54 -0500

    :art: not passing the test but the recipe one should work
    not passing the test but the recipe one should work

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,8 +3,19 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
-
+    rec = list(recipe.values())
+    ing = list(ingredients.values())
+    res = {rec[i]: ing[i] for i in range(0, len(rec))}
+    diff1 = []
+    for key, value in res.items():
+        if int(value) // int(key) > 1:
+            diff1.append(int(value // int(key)))
+    if len(diff1) == len(recipe.keys()):
+        return int(min(diff1))
+    else:
+        return 0
+        
+    
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
